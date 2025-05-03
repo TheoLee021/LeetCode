b278fd20472e33618105159a7c485ac746c82d35
Theo Lee <theolee021@gmail.com>
2025-05-02 21:59:45 -0700

    Create 3_valid_anagram.py

---
+++ 3_valid_anagram.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+        
+        hashmapS, hashmapT = {}, {}
+
+        for i in range(len(s)):
+            hashmapS[s[i]] = 1 + hashmapS.get(s[i], 0)
+            hashmapT[t[i]] = 1 + hashmapT.get(t[i], 0)
+        return hashmapS == hashmapT
