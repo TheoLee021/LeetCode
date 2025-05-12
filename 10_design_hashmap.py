0daae538f6f440892be56a405cd3348dca311eb6
Theo Lee <theolee021@gmail.com>
2025-05-11 22:07:28 -0700

    Create 10_design_hashmap.py

---
+++ 10_design_hashmap.py
@@ -0,0 +1,21 @@
+class ListNode:
+    def __init__(self, key=-1, val=-1, next=None):
+        self.key = key
+        self.val = val
+        self.next = next
+
+class MyHashMap:
+    def __init__(self):
+        self.map = [ListNode() for i in range(1000)]
+
+    def hash(self, key):
+        return key % len(self.map)
+    
+    def put(self, key: int, value: int):
+        cur = self.map[self.hash(key)]
+        while cur.next:
+            if cur.next.key == key:
+                cur.next.val = value
+                return
+            cur = cur.next
+        cur.next = ListNode(key, value)
