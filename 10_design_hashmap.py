319b45e239159eaefe1ed03810d734e3769a7f93
Theo Lee <theolee021@gmail.com>
2025-05-13 15:06:30 -0700

    Update 10_design_hashmap.py

--- 10_design_hashmap.py
+++ 10_design_hashmap.py
@@ -1,21 +1,57 @@
 class ListNode:
-    def __init__(self, key=-1, val=-1, next=None):
+    def __init__(self, key=-1, value=-1, next=None):
         self.key = key
-        self.val = val
+        self.val = value
         self.next = next
 
-class MyHashMap:
+class MyHashMap(object):
     def __init__(self):
         self.map = [ListNode() for i in range(1000)]
 
     def hash(self, key):
         return key % len(self.map)
-    
-    def put(self, key: int, value: int):
+
+    def put(self, key, value):
+        """
+        :type key: int
+        :type value: int
+        :rtype: None
+        """
         cur = self.map[self.hash(key)]
         while cur.next:
             if cur.next.key == key:
                 cur.next.val = value
                 return
             cur = cur.next
-        cur.next = ListNode(key, value)
+        cur.next = ListNode(key,value)
+
+    def get(self, key):
+        """
+        :type key: int
+        :rtype: int
+        """
+        cur = self.map[self.hash(key)]
+        while cur.next:
+            if cur.next.key == key:
+                return cur.next.val
+            cur = cur.next
+        return -1
+
+    def remove(self, key):
+        """
+        :type key: int
+        :rtype: None
+        """
+        cur = self.map[self.hash(key)]
+        while cur.next:
+            if cur.next.key == key:
+                cur.next = cur.next.next
+                return
+            cur = cur.next
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
