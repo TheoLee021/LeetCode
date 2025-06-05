a96dd9892145bf2462172bc21a3bf7dbc9283912
Theo Lee <theolee021@gmail.com>
2025-06-05 14:35:00 -0700

    Update 25_Valid_Palindrome2.py

--- 25_Valid_Palindrome2.py
+++ 25_Valid_Palindrome2.py
@@ -4,11 +4,18 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+        def is_palindrome(l, r):
+            while l < r:
+                if s[l] != s[r]:
+                    return False
+                l += 1
+                r -= 1
+            return True
+
         l, r = 0, len(s) - 1
 
         while l < r:
             if s[l] != s[r]:
-                skipL, skipR = s[l+1:r+1], s[l:r]
-                return (skipL == skipL[::-1] or skipR == skipR[::-1])
+                return (is_palindrome(l+1,r) or is_palindrome(l,r-1))
             l, r = l + 1, r - 1
         return True
