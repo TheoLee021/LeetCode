47ab6f4301dfaeb54fc4175e60894846ed56a50a
Theo Lee <theolee021@gmail.com>
2025-06-11 11:42:19 -0700

    Update 30_Three Sum.py

--- 30_Three Sum.py
+++ 30_Three Sum.py
@@ -27,6 +27,6 @@ class Solution(object):
                     r -= 1
                     while l < r and nums[l] == nums[l - 1]:
                         l += 1
-                    while l < r and nums[r] == nums[r + 1]:
-                        r -= 1
+                    # while l < r and nums[r] == nums[r + 1]:
+                    #     r -= 1
         return res
