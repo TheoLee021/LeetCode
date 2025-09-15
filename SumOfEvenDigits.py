03338e0b4bfc13c6997724c571443069485cb4a0
Theo Lee <theolee021@gmail.com>
2025-09-14 17:00:04 -0700

    Update SumOfEvenDigits.py

--- SumOfEvenDigits.py
+++ SumOfEvenDigits.py
@@ -1,7 +1,7 @@
 def solution(n):
     digit_sum = 0
     while n > 0:
-        digit = n % 10 # 
+        digit = n % 10 # extract the last digit
         if digit % 2 == 0:
             print(digit)
             digit_sum += digit
